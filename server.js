7ae765893468ff70c734e8b8778217ae0897dfea
Roman Gruba <roman.gruba@gmail.com>
2019-12-04 22:56:00 +0200

    uod

--- server.js
+++ server.js
@@ -1,2 +1,24 @@
 const http = require("http");
 const port = 3000;
+
+const server = http.createServer(function(req, res) {
+  if (req.method === "GET" && req.url === "/") {
+    res.setHeader("Content-Type", "text/html");
+    res.write(
+      '<form action="#"><input type="text"><button type="submit">Send</button></form>'
+    );
+    res.end();
+  }
+  if (req.url === "/favicon.ico") {
+    res.statusCode = 404;
+    res.end();
+  }
+});
+
+server.listen(port, err => {
+  if (err) {
+    return console.log("err :", err);
+  }
+  console.log(`Server is listening on port ${port}`);
+  // process.exit(1);
+});
