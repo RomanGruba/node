25165f99999cd413d1afd0b65e94b4c04cc2d170
Roman Gruba <roman.gruba@gmail.com>
2019-12-05 00:01:29 +0200

    up

--- server.js
+++ server.js
@@ -1,16 +1,27 @@
 const http = require("http");
 const port = 3000;
+const qs = require("querystring");
+const url = require("url");
 
 const server = http.createServer(function(req, res) {
-  if (req.method === "GET" && req.url === "/") {
+  if (req.url === "/favicon.ico") {
+    res.statusCode = 404;
+    res.end();
+  }
+
+  if (req.method === "GET") {
     res.setHeader("Content-Type", "text/html");
     res.write(
-      '<form action="#"><input type="text"><button type="submit">Send</button></form>'
+      '<form method="GET"><input type="text" name="name"><input type="text" name="dog"><button type="submit">Send</button></form>'
     );
     res.end();
   }
-  if (req.url === "/favicon.ico") {
-    res.statusCode = 404;
+  if (req.method === "POST") {
+    const myURL = url.parse(req.url);
+    const query = qs.parse(myURL.query);
+    console.log(query);
+
+    res.write(`Dog name - ${query.name}`);
     res.end();
   }
 });
