39c41ca6fd1e3f2eadc01ebfecda342b92810e4f
Roman Gruba <roman.gruba@gmail.com>
2019-12-07 12:36:52 +0200

    n3

--- app.js
+++ app.js
@@ -1,38 +1,19 @@
 const express = require("express");
 const app = express();
-const path = require("path");
-const multer = require("multer");
-const upload = multer({ dest: "uploads/" });
-const port = 5000;
+require("dotenv").config();
+const port = process.env.PORT || 5000;
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-// app.get(
-//   "/app",
-//   (req, res, next) => {
-//     const { is } = req.query;
-//     if (is) {
-//       next();
-//     } else {
-//       res.json({ message: `Add please query is = true ` });
-//     }
-//   },
-//   (req, res) => {
-//     res.json({ message: `Salute, is = ${req.query.is}` });
-//   }
-// );
-// app.use("/", (req, res) => {
-// const query = req.query;
-// console.log(query);
-// res.send("Hi!!!");
+app.set("view engine", "ejs");
 
-//   const body = req.body;
-//   console.log(body);
-//   res.json(body);
-// });
-
-app.post("/", upload.single(), (req, res) => {
-  res.sendFile(req.file.path);
+app.get("/", (req, res) => {
+  res.render(
+    "index",
+    {},
+    err => {
+      console.log(err);
+    },
+    ""
+  );
 });
 
-app.listen(port, () => console.log(`Server is on ${port}`));
+app.listen(port, () => console.log(`Server is running on port ${port}`));
